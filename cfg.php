650d6cdd89d175d94f06d7e3dbb9fac8b16dec89
zekiloi <80943695+zekiloi@users.noreply.github.com>
2021-11-29 21:30:20 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+$useragent = "Mozilla/5.0 (Linux; Android 8.1.0; iris65) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36";
+
+
+
+$ssid_ltc = "PHPSESSID=n0c1fgtlt588jr2jc9sudvtco0";
+
+$ssid_dgb = "PHPSESSID=cd696guail6u4u2l2ms0gvhk57";
+
+$ssid_trx = "PHPSESSID=htkf62dtg5dlsg3c06im3lecr3";
+
+$ssid_doge = "PHPSESSID=e040dka7cn5fd4554m8n3qogh2";
+
+$ssid_btc = "PHPSESSID=s02g258f1erg28gm86t3ltmfa5";
+
+$ssid_eth = "PHPSESSID=d06m5tlbij9kqqtpl0400s6881";
